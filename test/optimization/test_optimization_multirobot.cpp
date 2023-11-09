a775b1eaf1c5a9704f9f71d6e02007d2970a21d2
Quim Ortiz - Work <quimortiz21@gmail.com>
2023-11-09 19:06:37 +0100

    add the test with bug in another branch

--- test/optimization/test_optimization_multirobot.cpp
+++ test/optimization/test_optimization_multirobot.cpp
@@ -440,3 +440,81 @@ BOOST_AUTO_TEST_CASE(t_hetero_random_2) {
   multi_out.to_yaml_format("/tmp/test_gen_p10_n2_1_hetero_solution.yaml");
 }
 
+
+// needs to be debugged
+BOOST_AUTO_TEST_CASE(t_gen_p10_n8_9_hetero) {
+
+  bool sum_robots_cost = 1;
+  std::string dynobench_base = "../../dynobench/";
+  std::string env_file =
+      dynobench_base + "envs/multirobot/example/gen_p10_n8_9_hetero.yaml";
+  std::string initial_guess_file =
+      dynobench_base + "envs/multirobot/results/gen_p10_n8_9_hetero_db.yaml";
+
+  Problem problem(env_file);
+  MultiRobotTrajectory init_guess_multi_robot;
+  init_guess_multi_robot.read_from_yaml(initial_guess_file.c_str());
+
+  std::vector<int> goal_times(init_guess_multi_robot.trajectories.size());
+
+  std::transform(init_guess_multi_robot.trajectories.begin(),
+                 init_guess_multi_robot.trajectories.end(), goal_times.begin(),
+                 [](const Trajectory &traj) { return traj.states.size(); });
+
+  std::cout << "goal times are " << std::endl;
+  for (auto &t : goal_times) {
+    std::cout << t << std::endl;
+  }
+
+  Trajectory init_guess;
+  if (sum_robots_cost) {
+    std::cout
+        << "warning: new approach where each robot tries to reach the goal fast"
+        << std::endl;
+    problem.goal_times = goal_times;
+  }
+
+  else {
+    std::cout
+        << "warning: old apprach, robots will reach the goals at the same time "
+        << std::endl;
+  }
+
+  Options_trajopt options_trajopt;
+  options_trajopt.solver_id = 1;
+  options_trajopt.control_bounds = 1;
+  options_trajopt.use_warmstart = 1;
+  options_trajopt.weight_goal = 100;
+  options_trajopt.max_iter = 50;
+  problem.models_base_path = dynobench_base + std::string("models/");
+
+  Result_opti result;
+  Trajectory sol;
+
+  dynobench::Trajectory init_guess_joint =
+      init_guess_multi_robot.transform_to_joint_trajectory();
+  init_guess_joint.to_yaml_format("/tmp/check6.yaml");
+
+  trajectory_optimization(problem, init_guess_joint, options_trajopt, sol,
+                          result);
+
+  BOOST_TEST(result.feasible == 1);
+
+  std::cout << "optimization done! " << std::endl;
+  std::vector<int> index_time_goals;
+
+  if (problem.goal_times.size()) {
+    index_time_goals = sol.multi_robot_index_goal;
+  } else {
+    size_t num_robots = init_guess_multi_robot.get_num_robots();
+    index_time_goals = std::vector<int>(num_robots, sol.states.size());
+  }
+
+  MultiRobotTrajectory multi_out = from_joint_to_indiv_trajectory(
+      sol, init_guess_multi_robot.get_nxs(), init_guess_multi_robot.get_nus(),
+      index_time_goals);
+
+  multi_out.to_yaml_format("/tmp/test_gen_p10_n8_9_hetero_solution.yaml");
+}
+
+
