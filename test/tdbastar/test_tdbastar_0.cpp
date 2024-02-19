8374dab144a9fd754e4684dec9632713e7bd0d32
akmaralAW <moldagalieva@tu-berlin.de>
2024-02-19 16:53:33 +0100

    reverse search with a single robot, fix robot names for dynobench test

--- test/tdbastar/test_tdbastar_0.cpp
+++ test/tdbastar/test_tdbastar_0.cpp
@@ -14,7 +14,6 @@
 #define DYNOBENCH_BASE "../dynobench/"
 #define BASE_PATH_MOTIONS "../../new_format_motions/"
 
-
 using namespace dynoplan;
 using namespace dynobench;
 // Run from dynoplan build
@@ -96,86 +95,45 @@ BOOST_AUTO_TEST_CASE(test_eval_multiple) {
 // for reverse search
 BOOST_AUTO_TEST_CASE(test_eval_multiple_reverse) {
 
-  std::vector<Problem> problems{
-      DYNOBENCH_BASE "envs/unicycle1_v0/swap/swap1_unicycle.yaml",
-      DYNOBENCH_BASE "envs/unicycle2_v0/swap/swap1_unicycle2.yaml",
-      DYNOBENCH_BASE "envs/integrator2_2d_v0/swap/swap1_double_integrator.yaml",
-      DYNOBENCH_BASE "envs/car1_v0/swap/swap1_trailer.yaml"};
-
-  for (auto &p : problems) {
-    p.models_base_path = DYNOBENCH_BASE "models/";
-  }
-
-  Options_tdbastar o_uni1, o_uni2, o_integrator2, o_car;
+  Problem problem (DYNOBENCH_BASE "envs/unicycle1_v0/swap/swap1_unicycle.yaml");
+  problem.models_base_path = DYNOBENCH_BASE "models/";
+  std::string msg = problem.name;
+  Options_tdbastar o_uni1;
 
   o_uni1.max_motions = 100;
   o_uni1.delta = .5;
   o_uni1.motionsFile =
       BASE_PATH_MOTIONS "unicycle1_v0/unicycle1_v0.msgpack";
+  o_uni1.search_timelimit = 40 * 10e3;
 
-  o_uni2.max_motions = 100;
-  o_uni2.delta = .5;
-  o_uni2.motionsFile =
-      BASE_PATH_MOTIONS "unicycle2_v0/unicycle2_v0.msgpack";
-
-  o_integrator2.max_motions = 100;
-  o_integrator2.delta = .5;
-  o_integrator2.motionsFile =
-      BASE_PATH_MOTIONS "integrator2_2d_v0/integrator2_2d_v0.msgpack";
-
-  o_car.max_motions = 400;
-  o_car.delta = .75;
-  o_car.motionsFile =
-      BASE_PATH_MOTIONS "car1_v0/car1_v0.msgpack";
-
-
-  std::vector<Options_tdbastar> options{
-      o_uni1, o_uni2, o_integrator2, o_car};
-
-  for (auto &o : options) {
-    o.search_timelimit = 40 * 10e3;
-  }
-
-  // you can choose the heuristic here!!
-
-  DYNO_CHECK_EQ(options.size(), problems.size(), AT);
   size_t robot_id = 0;
+  size_t robot_num = 1;
   std::vector<dynobench::Trajectory> expanded_trajs_tmp;
   // save expanded nodes
-  std::string output_folder = "../reverse_expansion_vis";
-  create_folder_if_necessary(output_folder);
-  for (size_t j = 0; j < options.size(); j++) {
+  // std::string output_folder = "../reverse_expansion_vis";
+  // create_folder_if_necessary(output_folder);
+  std::vector<ompl::NearestNeighbors<std::shared_ptr<AStarNode>>*> all_heuristics(robot_num, nullptr);
+  Trajectory traj_out;
+  Out_info_tdb info_out;
+  std::vector<Motion> motions;
+  std::shared_ptr<dynobench::Model_robot> robot = dynobench::robot_factory(
+      (problem.models_base_path + problem.robotType + ".yaml").c_str(),
+      problem.p_lb, problem.p_ub);
 
-    auto &problem = problems[j];
-    auto &option = options[j];
+  load_motion_primitives_new(o_uni1.motionsFile, *robot, motions,
+                            o_uni1.max_motions, o_uni1.cut_actions, false,
+                            o_uni1.check_cols);
 
-    std::string msg = problem.name;
+  o_uni1.motions_ptr = &motions;
+  BOOST_REQUIRE_NO_THROW(tdbastar(problem, o_uni1, traj_out, /*constraints*/{}, 
+              info_out, robot_id, /*reverse_search*/true, expanded_trajs_tmp, nullptr, &all_heuristics[robot_id]));
 
-    Trajectory traj_out;
-    Out_info_tdb info_out;
-
-    // load motions
-
-    std::vector<Motion> motions;
-
-    std::shared_ptr<dynobench::Model_robot> robot = dynobench::robot_factory(
-        (problem.models_base_path + problem.robotType + ".yaml").c_str(),
-        problem.p_lb, problem.p_ub);
-
-    load_motion_primitives_new(option.motionsFile, *robot, motions,
-                               option.max_motions, option.cut_actions, false,
-                               option.check_cols);
-
-    option.motions_ptr = &motions;
-    expanded_trajs_tmp.clear();
-    BOOST_REQUIRE_NO_THROW(tdbastar(problem, option, traj_out, /*constraints*/{}, 
-                info_out, robot_id, /*reverse_search*/true, expanded_trajs_tmp, nullptr, nullptr));
-
-    // BOOST_TEST(info_out.solved, msg);
-    
-    std::ofstream out2(output_folder + "/exp_trajs_reverse_" + problem.robotType + ".yaml"); // assumes different robot types
-    export_node_expansion(expanded_trajs_tmp, &out2);
-  }
+  // save and visualize expanded nodes
+  // std::ofstream out2(output_folder + "/exp_trajs_reverse_" + problem.robotType + ".yaml"); // assumes different robot types
+  // export_node_expansion(expanded_trajs_tmp, &out2);
+  BOOST_REQUIRE_NO_THROW(tdbastar(problem, o_uni1, traj_out, /*constraints*/{}, 
+              info_out, robot_id, /*reverse_search*/false, expanded_trajs_tmp, all_heuristics[robot_id], nullptr));
+  BOOST_TEST(info_out.solved, msg);
 }
 
 BOOST_AUTO_TEST_CASE(test_eval_multiple_with_constraints) {
