4933d882c00b14a21dcaf2140ff79535ef4a3ced
akmaralAW <moldagalieva@tu-berlin.de>
2024-05-22 17:42:20 +0200

    formatting the code, debug rewiring with quim

--- include/dynoplan/tdbastar/tdbastar_epsilon.hpp
+++ include/dynoplan/tdbastar/tdbastar_epsilon.hpp
@@ -77,6 +77,14 @@ int lowLevelfocalHeuristicState(
     const float current_gScore,
     std::vector<fcl::CollisionObjectd *> &robot_objs, bool reachesGoal = false);
 
+int lowLevelfocalHeuristicSingleState(
+    std::vector<LowLevelPlan<dynobench::Trajectory>> &solution,
+    const std::vector<std::shared_ptr<dynobench::Model_robot>> &all_robots,
+    Eigen::VectorXd state1, size_t &current_robot_idx,
+    const float current_gScore,
+    std::vector<fcl::CollisionObjectd *> &robot_objs, bool reachesGoal = false);
+
+
 int lowLevelfocalHeuristicStateDebug(
     std::vector<LowLevelPlan<dynobench::Trajectory>> &solution,
     const std::vector<std::shared_ptr<dynobench::Model_robot>> &all_robots,
