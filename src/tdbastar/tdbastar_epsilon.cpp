25bed175a236f274c02007bff92c99c72bdfe768
akmaralAW <moldagalieva@tu-berlin.de>
2024-06-11 16:56:06 +0200

    update current_arr_idx after the solution

--- src/tdbastar/tdbastar_epsilon.cpp
+++ src/tdbastar/tdbastar_epsilon.cpp
@@ -711,22 +711,9 @@ namespace dynoplan
         std::cout << "x: " << best_node->state_eig.format(FMT) << std::endl;
         std::cout << "d: " << distance_to_goal << std::endl;
         std::cout << "focal: " << best_node_bestFocalHeuristic << std::endl;
-        if (all_print){
-          std::cout << "Inspecting the solution!" << std::endl;
-          std::shared_ptr<AStarNode> n = best_node;
-          size_t arrival_idx = n->best_focal_arrival_idx;
-          std::cout << n->state_eig.format(dynobench::FMT) << std::endl;
-          while (n != nullptr) {
-            std::cout << "//////////////////" << std::endl;
-            std::cout << "arrival_id: " << arrival_idx << std::endl;
-            const auto &arrival = n->arrivals[arrival_idx]; // get the struct, exact one using arrival_idx
-            n = arrival.came_from; // get the parent
-            if (n != nullptr){
-              std::cout << "came_from: " << n->state_eig.format(dynobench::FMT) << std::endl;
-            }
-            arrival_idx = arrival.arrival_idx; // get the best_node.current_arrival_idx in order to get the exact element of the current node's arrivals vector.
-          }
-        }
+        // for the consistency in order to be able to use the same from_solution_to_yaml_and_traj function. 
+        // With focal search current_arrival_idx doesn't provide any useful info.
+        best_node->current_arrival_idx = best_node->best_focal_arrival_idx;
         status = Terminate_status::SOLVED;
         break;
       }
