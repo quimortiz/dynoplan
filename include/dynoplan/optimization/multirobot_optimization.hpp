1de365406543c2ea18cfeb94f058213f390eb162
akmaralAW <moldagalieva@tu-berlin.de>
2026-05-17 21:48:18 +0200

    mrs optimization changes in dynobench

--- include/dynoplan/optimization/multirobot_optimization.hpp
+++ include/dynoplan/optimization/multirobot_optimization.hpp
@@ -8,7 +8,7 @@
 bool execute_optimizationMultiRobot(const std::string &env_file,
                                     const std::string &initial_guess_file,
                                     const std::string &output_file,
-                                    const std::string &dynobench_base,
+                                    const std::string &base,
                                     bool sum_robots_cost);
 
 bool execute_optimizationMetaRobot(
